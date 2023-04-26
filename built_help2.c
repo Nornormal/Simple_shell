31dd52fa84d8837d88d759c5462483ad5ad431d6
Sara Edrees <122927746+Saraedrees@users.noreply.github.com>
2023-04-27 01:48:10 +0200

    Update built_help2.c

--- built_help2.c
+++ built_help2.c
@@ -1,40 +1,40 @@
 #include "shell.h"
 
 /**
- * help_env - Displays information on the shellby builtin command 'env'.
+ * hlp_env - function prints information on the built-in shell_cmd command 'env'.
  */
-void help_env(void)
+void hlp_env(void)
 {
-	char *msg = "env: env\n\tPrints the current environment.\n";
+	char *outp = "env: env\n\t Prints the current environment..\n";
 
-	write(STDOUT_FILENO, msg, _strlen(msg));
+	write(STDOUT_FILENO, outp, _strlen(outp));
 }
 
 /**
- * help_setenv - Displays information on the shellby builtin command 'setenv'.
+ * hlp_setenv - function prints information on the built-in shell_cmd command 'setenv'.
  */
-void help_setenv(void)
+void hlp_setenv(void)
 {
-	char *msg = "setenv: setenv [VARIABLE] [VALUE]\n\tInitializes a new";
+	char *outp = "setenv: setenv [VARIABLE] [VALUE]\n\t Initializes a new";
 
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "environment variable, or modifies an existing one.\n\n";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "\tUpon failure, prints a message to stderr.\n";
-	write(STDOUT_FILENO, msg, _strlen(msg));
+	write(STDOUT_FILENO, outp, _strlen(outp));
+	outp = "environment variable, or modifies an existing one..\n\n";
+	write(STDOUT_FILENO, outp, _strlen(outp));
+	outp = "\t Upon failure, prints a message to stderr..\n";
+	write(STDOUT_FILENO, outp, _strlen(outp));
 }
 
 /**
- * help_unsetenv - Displays information on the shellby builtin command
- * 'unsetenv'.
+ * help_unsetenv - function prints information on the built-in shell_cmd command
+ * 'unsetenv'..
  */
 void help_unsetenv(void)
 {
-	char *msg = "unsetenv: unsetenv [VARIABLE]\n\tRemoves an ";
+	char *outp = "unsetenv: unsetenv [VARIABLE]\n\t Removes an ";
 
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "environmental variable.\n\n\tUpon failure, prints a ";
-	write(STDOUT_FILENO, msg, _strlen(msg));
-	msg = "message to stderr.\n";
-	write(STDOUT_FILENO, msg, _strlen(msg));
+	write(STDOUT_FILENO, outp, _strlen(outp));
+	outp = "environmental variable..\n\n\t Upon failure, prints a ";
+	write(STDOUT_FILENO, outp, _strlen(outp));
+	outp = "message to stderr..\n";
+	write(STDOUT_FILENO, outp, _strlen(outp));
 }
