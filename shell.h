b3c25b8c0cca9990f5c70ad657fe9796d1fa40fd
Nornormal <122840025+Nornormal@users.noreply.github.com>
2023-04-27 02:40:20 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -15,63 +15,59 @@
 #define END_OF_FILE -2
 #define EXIT -3
 
-/* Global environemnt */
 extern char **environ;
-/* Global program name */
-char *name;
-/* Global history counter */
-int hist;
+char *nme;
+int hst;
 
 /**
- * struct list_s - A new struct type defining a linked list.
+ * struct list_s - A new struct define a linked list.
  * @dir: A current directory path.
  * @next: A pointer to next struct list_s.
  */
 typedef struct list_s
 {
 	char *dir;
-	struct list_s *next;
+	struct list_s *nxt;
 } list_t;
 
 /**
- * struct built-in_s - A new struct type defining built-in commands.
- * @name: The name of the built-in command.
- * @f: A function pointer to the builtin command's function.
- */
-typedef struct builtin_s
-{
-	char *name;
-	int (*f)(char **av, char **frnt);
-} builtin_t;
-
-/**
- * struct alias_s - A new struct defining aliases.
- * @name: The name of the alias.
- * @value: The value of the alias.
- * @next: A pointer to another struct alias_s.
+ * struct alias_s - A new struct define alias.
+ * @nme: The name of the alias.
+ * @val: The value of the alias.
+ * @nxt: pointer to next struct alias_s.
  */
 typedef struct alias_s
 {
-	char *name;
-	char *value;
-	struct alias_s *next;
+	char *val;
+	char *nme;
+	struct alias_s *nxt;
 } alias_t;
 
-/* Global aliases linked list */
-alias_t *aliases;
+alias_t *alias;
 
-/* Input Helpers */
+
+/**
+ * struct builtin_s - new struct define built-in commands.
+ * @name: name of the built-in command.
+ * @f: function pointer to the builtin command's function.
+ */
+typedef struct builtin_s
+{
+	int (*f)(char **av, char **frnt);
+	char *nme;
+} builtin_t;
+
 void handle_line(char **lne, ssize_t rread);
-ssize_t get_new_len(char *lne);
-void logical_ops(char *lne, ssize_t *n_len);
-void variable_replacement(char **ag, int *exe_retn);
-char *get_args(char *lne, int *exe_retn);
-int call_args(char **ag, char **front, int *exe_retn);
-int run_args(char **ag, char **front, int *exe_retn);
-int handle_args(int *exe_retn);
-int check_args(char **ag);
-void free_args(char **args, char **front);
-char **replace_aliases(char **ag);
+ssize_t g_n_len(char *lne);
+void logic_op(char *lne, ssize_t *n_len);
+void var_replace(char **ag, int *ex_retn);
+char *g_ag(char *lne, int *ex_retn);
+int call_ag(char **ag, char **frnt, int *ex_retn);
+int run_ag(char **ag, char **frnt, int *ex_retn);
+int handle_ag(int *ex_retn);
+int check_ag(char **ag);
+void fre_ag(char **ag, char **frnt);
+char **replace_alias(char **ag);
 
 void hlp_setenv(void);
 void hlp_unsetenv(void);
@@ -83,45 +79,40 @@ void hlp_alias(void);
 void hlp_cd(void);
 void hlp_env(void);
 
-/* String functions */
-int _strlen(const char *s);
-char *_strcat(char *dest, const char *src);
-char *_strncat(char *dest, const char *src, size_t n);
 char *_strcpy(char *dest, const char *src);
 char *_strchr(char *s, char c);
 int _strspn(char *s, char *accept);
+int _strlen(const char *s);
+char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, const char *src, size_t n);
 int _strcmp(char *s1, char *s2);
 int _strncmp(const char *s1, const char *s2, size_t n);
 
-/* Main Helpers */
-char *g_location(char *command);
+char *g_location(char *cmd);
 list_t *g_path_dir(char *path);
-int execute(char **ag, char **front);
-void fre_list(list_t *head);
+int execute(char **ag, char **frnt);
+void fre_list(list_t *hd);
 void *_realloc(void *pnt, unsigned int o_size, unsigned int n_size);
 ssize_t _getline(char **lpnt, size_t *n, FILE *r_stream);
 char **_strtok(char *lne, char *delim);
-char *_itoa(int num);
+char *_itoa(int nmb);
 
-/* Builtins */
-int (*get_builtin(char *command))(char **ag, char **frnt);
+int (*get_builtin(char *cmd))(char **ag, char **frnt);
 int shell_cmd_exit(char **ag, char **front);
 int shell_cmd_setenv(char **ag, char __attribute__((__unused__)) **frnt);
 int shell_cmd_unsetenv(char **ag, char __attribute__((__unused__)) **frnt);
 int shell_cmd_cd(char **ag, char __attribute__((__unused__)) **frnt);
 int shell_cmd_alias(char **ag, char __attribute__((__unused__)) **frnt);
 int shell_cmd_env(char **ag, char __attribute__((__unused__)) **frnt);
-void set_alias(char *var_name, char *value);
+void set_alias(char *var_nme, char *val);
 void print_alias(alias_t *alias);
 int shell_cmd_hlp(char **ag, char __attribute__((__unused__)) **frnt);
 
 
-/* Builtin Helpers */
 char **_cpenv(void);
 void fre_env(void);
 char **_getenv(const char *var);
 
-/* Error Handling */
 int create_error(char **ag, int err);
 char *error_env(char **ag);
 char *error_1(char **ag);
@@ -131,12 +122,11 @@ char *error_2_syntax(char **ag);
 char *error_126(char **ag);
 char *error_127(char **ag);
 
-/* Linkedlist Helpers */
 alias_t *add_alias_end(alias_t **hd, char *nme, char *val);
 void fre_alias_list(alias_t *hd);
 list_t *add_node_end(list_t **hd, char *dir);
 void fre_list(list_t *hd);
 
 
-int proc_file_commands(char *f_path, int *exe_retn);
+int proc_file_commands(char *f_path, int *ex_retn);
 #endif /* _SHELL_H_ */
