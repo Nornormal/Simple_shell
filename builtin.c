f9a50a0030bf860f9ecda71b592b60cbe875eb6b
Sara Edrees <122927746+Saraedrees@users.noreply.github.com>
2023-04-27 21:03:15 +0200

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -94,7 +94,7 @@ int shellby_cd(char **ag, char __attribute__((__unused__)) **frnt)
 	char *o_pwd = NULL, *pwd = NULL;
 	struct stat dir;
 
-	o_pwd = g_cwd(o_pwd, 0);
+	o_pwd = get_cwd(o_pwd, 0);
 	if (!o_pwd)
 		return (-1);
 
@@ -132,7 +132,7 @@ int shellby_cd(char **ag, char __attribute__((__unused__)) **frnt)
 			chdir(*(_getenv("HOME")) + 5);
 	}
 
-	pwd = g_cwd(pwd, 0);
+	pwd = get_cwd(pwd, 0);
 	if (!pwd)
 		return (-1);
 
