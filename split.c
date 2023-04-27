83cf1bcb8719c6f6b1b38a9e4940ffe40b82d952
Nornormal <122840025+Nornormal@users.noreply.github.com>
2023-04-27 03:51:54 +0200

    Update split.c

--- split.c
+++ split.c
@@ -1,100 +1,97 @@
 #include "shell.h"
 
 /**
- * token_len - Locates the delimiter index marking the end
- *             of the first token contained within a string.
- * @str: The string to be searched.
- * @delim: The delimiter character.
+ * tok_ln - func locates the delimiter index.
+ * @str: string to be searched.
+ * @delim: delimiter character.
  *
- * Return: The delimiter index marking the end of
- *         the intitial token pointed to be str.
+ * Return: delimiter index marking the end of str.
  */
-int token_len(char *str, char *delim)
+int tok_ln(char *str, char *delim)
 {
-	int index = 0, len = 0;
+	int idx = 0, ln = 0;
 
-	while (*(str + index) && *(str + index) != *delim)
+	while (*(str + idx) && *(str + idx) != *delim)
 	{
-		len++;
-		index++;
+		ln++;
+		idx++;
 	}
 
-	return (len);
+	return (ln);
 }
 
 /**
- * count_tokens - Counts the number of delimited
- *                words contained within a string.
- * @str: The string to be searched.
- * @delim: The delimiter character.
+ * cnt_tok - func counts the number of delimited words
+ * @str: string to be searched.
+ * @delim: delimiter character.
  *
- * Return: The number of words contained within str.
+ * Return: number of words within str.
  */
-int count_tokens(char *str, char *delim)
+int cnt_tok(char *str, char *delim)
 {
-	int index, tokens = 0, len = 0;
+	int idx, tok = 0, ln = 0;
 
-	for (index = 0; *(str + index); index++)
-		len++;
+	for (idx = 0; *(str + idx); idx++)
+		ln++;
 
-	for (index = 0; index < len; index++)
+	for (idx = 0; idx < ln; idx++)
 	{
-		if (*(str + index) != *delim)
+		if (*(str + idx) != *delim)
 		{
-			tokens++;
-			index += token_len(str + index, delim);
+			tok++;
+			idx += tok_ln(str + idx, delim);
 		}
 	}
 
-	return (tokens);
+	return (tok);
 }
 
 /**
- * _strtok - Tokenizes a string.
- * @line: The string.
- * @delim: The delimiter character to tokenize the string by.
+ * _strtok - fun tokenize a string.
+ * @line: string.
+ * @delim: delimiter char to tokenize the string by.
  *
- * Return: A pointer to an array containing the tokenized words.
+ * Return: pointer to an array of the tokenized words.
  */
-char **_strtok(char *line, char *delim)
+char **_strtok(char *lne, char *delim)
 {
-	char **ptr;
-	int index = 0, tokens, t, letters, l;
+	char **pnt;
+	int idx = 0, tok, t, letter, l;
 
-	tokens = count_tokens(line, delim);
-	if (tokens == 0)
+	tok = cnt_tok(line, delim);
+	if (tok == 0)
 		return (NULL);
 
-	ptr = malloc(sizeof(char *) * (tokens + 2));
-	if (!ptr)
+	pnt = malloc(sizeof(char *) * (tok + 2));
+	if (!pnt)
 		return (NULL);
 
-	for (t = 0; t < tokens; t++)
+	for (t = 0; t < tok; t++)
 	{
-		while (line[index] == *delim)
-			index++;
+		while (lne[idx] == *delim)
+			idx++;
 
-		letters = token_len(line + index, delim);
+		letter = tok_ln(lne + idx, delim);
 
-		ptr[t] = malloc(sizeof(char) * (letters + 1));
-		if (!ptr[t])
+		pnt[t] = malloc(sizeof(char) * (letter + 1));
+		if (!pnt[t])
 		{
-			for (index -= 1; index >= 0; index--)
-				free(ptr[index]);
-			free(ptr);
+			for (idx -= 1; idx >= 0; idx--)
+				free(pnt[idx]);
+			free(pnt);
 			return (NULL);
 		}
 
-		for (l = 0; l < letters; l++)
+		for (l = 0; l < letter; l++)
 		{
-			ptr[t][l] = line[index];
-			index++;
+			pnt[t][l] = lne[idx];
+			idx++;
 		}
 
-		ptr[t][l] = '\0';
+		pnt[t][l] = '\0';
 	}
-	ptr[t] = NULL;
-	ptr[t + 1] = NULL;
+	pnt[t] = NULL;
+	pnt[t + 1] = NULL;
 
-	return (ptr);
+	return (pnt);
 }
